f60af4d03c216c75ddc8de1e1e1c9551f3db7d97
logicbreaker <ali.kabeel@gmail.com>
2020-04-29 21:05:43 +0200

    Simple Chatty Bot

---
+++ Simple Chatty Bot/Stage1.py
@@ -0,0 +1,2 @@
+print("""Hello! My name is logic.
+I was created in 2020.""")
---
+++ Simple Chatty Bot/Stage2.py
@@ -0,0 +1,7 @@
+print('Hello! My name is logic.')
+print('I was created in 2020.')
+print('Please, remind me your name.')
+your_name = input()
+# reading a name
+
+print('What a great name you have, '+ your_name +'!')
